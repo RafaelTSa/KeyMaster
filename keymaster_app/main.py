db2d1c442940ef8e4f6a40afebff63a7ad749da0
Rafael T Santos <texera09@gmail.com>
2025-12-01 22:28:26 -0300

    adiciona estrura while para pedir input e converte minusculas e remove espaço em branco

--- keymaster_app/main.py
+++ keymaster_app/main.py
@@ -5,9 +5,8 @@ import random  # Gerador de números aleatórios.
 import secrets  # Gerador de números aleatórios seguros.
 import sys # Usado para fechar o programa de forma controlada se o usuário cometer um erro grave.
 
-def main():
-    """Função principal que será executada."""
-    print("KeyMaster: Gerador de Senhas Seguras (Estrutura Base)")
-
+def obter_opcao_sim_nao(pergunta): #Função auxiliar para garantir que o usuário responda com 's' (sim) ou 'n' (não).
+    while True: # Pede a entrada, converte para minúsculas e remove espaços em branco (strip)
+        reposta = input (f"{pergunta} (s/n: ").lower().strip()
 if __name__ == "__main__":
     main()
