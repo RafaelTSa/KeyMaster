584f44a725c608eee13572d66fb45eaad2ee5d19
Rafael T Santos <texera09@gmail.com>
2025-12-02 06:21:30 -0300

    adiciona FUNÇÃO PRINCIPAL DE GERAÇÃO

--- keymaster_app/main.py
+++ keymaster_app/main.py
@@ -28,3 +28,34 @@ def obter_tamanho_senha(): # Função para obter o tamanho desejado da senha, ga
         except ValueError: # verifica se o usuário digitar algo que não é um número
             print("Entrada inválida. Por favor, digite um número inteiro válido.")
 
+# 2. Função Principal de Geração:
+
+def gerar_senha(tamanho, usar_maiusculas, usar_numeros, usar_simbolos): #Gera a senha segura usando parâmetros.
+    pool_total = list(string.ascii_lowercase) # Base obrigatória: começa sempre com letras minúsculas.
+    pools_requeridas = [string.ascii_lowercase] # Lista dos conjuntos que precisamos para garantir pelo menos um caractere
+
+    if usar_maiusculas: # adiciona escolha p/ usuário, se True
+        pool_total.extend(string.ascii_uppercase)
+        pools_requeridas.append(string.ascii_uppercase)
+
+    if usar_numeros:
+        pool_total.extend(string.digits)
+        pools_requeridas.append(string.digits)
+
+    if usar_simbolos:
+        pool_total.extend(string.punctuation)
+        pools_requeridas.append(string.punctuation)
+
+    senha = [] # Gera e gerante a composição
+    for pool in pools_requeridas: # Garante a composição mínima (Pega 1 char de cada pool requerida)
+        senha.append(random.choice(pool))
+
+    num_restante = tamanho - len(senha) # Calcula quantos caracteres Faltam.
+
+    for _ in range(num_restante): # Adiciona caracteres aleatórios.
+        senha.append(random.choice(pool_total))
+
+    random.shuffle(senha) # Embaralha a senha
+
+    return "".join(senha)
+
